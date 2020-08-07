7439c23ac4cbab49ab2fb92499513f8ce13f41ca
Lewis Monteith <lewismonteith@gmail.com>
2020-08-07 16:51:22 +0100

    Dont immediately hide card

--- hackerstash/static/js/previews.js
+++ hackerstash/static/js/previews.js
@@ -1,13 +1,30 @@
 const previews = document.querySelectorAll('.preview');
 
+function removePreviewCard() {
+    if (window.card) {
+        document.body.removeChild(window.card);
+        delete window.card;
+    }
+}
+
 previews.forEach((preview) => {
     preview.addEventListener('mouseenter', (event) => {
+        removePreviewCard()
+
         const element = event.target.closest('.preview')
         const data = JSON.parse(element.getAttribute('data-preview'));
         const coords = element.getBoundingClientRect();
         const card = document.createElement('div');
 
         card.classList.add('preview-card');
+
+        const lists = data.lists.map(l => `
+            <li>
+                <p class="small">${l.key}</p>
+                <p class="small truncate">${l.value}</p>
+            </li>
+        `).join('');
+
         card.innerHTML = `
             <div class="card-header">
                 <span class="avatar">
@@ -17,22 +34,36 @@ previews.forEach((preview) => {
             </div>
             <p class="small">${data.description}</p>
             <ul class="display-options">
-                ${data.lists.map(l => `
-                    <li>
-                        <p class="small">${l.key}</p>
-                        <p class="small truncate">${l.value}</p>
-                    </li>
-                `).join('')}
+                ${lists}
             </ul>
         `;
         card.style.left = coords.left + 'px';
         card.style.top = (coords.top + 40) + 'px';
+
+        card.addEventListener('mouseleave', (event) => {
+            removePreviewCard();
+        });
 
         document.body.appendChild(card);
         window.card = card;
     });
 
     preview.addEventListener('mouseleave', (event) => {
-        if (window.card) document.body.removeChild(window.card);
+        if (window.cardtimeout) {
+            clearTimeout(window.cardtimeout);
+            delete window.cardtimeout;
+        }
+
+        window.cardtimeout = setTimeout(() => {
+            const hoveredElements = Array.from(document.querySelectorAll(':hover'));
+
+            // Mouse has moved off screen
+            if (hoveredElements.length === 0) {
+                return removePreviewCard();
+            }
+
+            const isHoveredOverCard = !!hoveredElements.pop().closest('.preview-card');
+            if (!isHoveredOverCard) removePreviewCard();
+        }, 1000);
     });
 });
