699211b315f984be87d3b4de294605bc2916d815
Lewis Monteith <lewismonteith@gmail.com>
2020-08-09 10:18:11 +0100

    Try proxyfix

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,8 @@
+from werkzeug.middleware.proxy_fix import ProxyFix
 from hackerstash.server import create_app
 
 app = create_app()
+app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_host=1)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
