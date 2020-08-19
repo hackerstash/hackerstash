13c6a453e12b029652bd79080b9e2ad83331c643
Lewis Monteith <lewismonteith@gmail.com>
2020-08-19 08:50:22 +0100

    Fix asset build issue

--- hackerstash/utils/assets.py
+++ hackerstash/utils/assets.py
@@ -23,7 +23,7 @@ scripts = [
 ]
 
 js = Bundle(*scripts, output='out/main.js')
-scss = Bundle(*styles, filters='pyscss', output='out/main.css')
+scss = Bundle(*styles, filters='pyscss', output='out/main.css', depends='**/*.scss')
 
 assets.register('js_all', js)
 assets.register('scss_all', scss)
