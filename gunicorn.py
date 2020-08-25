a7478853cd8795ce6b0c760852172e4c73faa8d5
Lewis Monteith <lewismonteith@gmail.com>
2020-08-25 14:18:16 +0100

    Fix comment color and use miliseconds for response time

--- gunicorn.py
+++ gunicorn.py
@@ -5,4 +5,4 @@ workers = 2
 threads = 4
 reload = os.environ.get('DEBUG', False)
 accesslog = '-'
-access_log_format = '{"address":"%(h)s", "method":"%(m)s", "url":"%(U)s", "status":%(s)s, "duration":%(T)s, "pid":"%(p)s"}'
+access_log_format = '{"address":"%(h)s", "method":"%(m)s", "url":"%(U)s", "status":%(s)s, "duration":%(M)s, "pid":"%(p)s"}'
