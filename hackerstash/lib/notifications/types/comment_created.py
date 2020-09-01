81631b0465245d7e3e7cf1df6ee7e2241b2e98cf
Lewis Monteith <lewismonteith@gmail.com>
2020-09-01 20:42:16 +0100

    Fix replying notification on your own post

--- hackerstash/lib/notifications/types/comment_created.py
+++ hackerstash/lib/notifications/types/comment_created.py
@@ -9,30 +9,29 @@ class CommentCreated(Base):
 
         comment = payload['comment']
 
-        # Don't notify yourself that you commented
-        # on your own post
-        if comment.post.user.id != g.user.id:
-            # If the comment has no parent then the post author should
-            # recieve a notification. Otherwise the owner of the parent
-            # comment should receive it instead.
-            if comment.parent_comment_id:
-                parent_comment = Comment.query.get(comment.parent_comment_id)
+        # If the comment has no parent then the post author should
+        # recieve a notification. Otherwise the owner of the parent
+        # comment should receive it instead.
+        if comment.parent_comment_id:
+            parent_comment = Comment.query.get(comment.parent_comment_id)
 
-                # Likewise, you don't want to know that
-                # you replied to yourself
-                if parent_comment.user.id != g.user.id:
-                    self.notifications_to_send.append({
-                        'user': parent_comment.user,
-                        'payload': payload,
-                        'email_type': 'replied_to_comment',
-                        'notification_type': 'someone_replies_to_your_comment',
-                        'notification_message': self.render_notification_message('someone_replies_to_your_comment')
-                    })
-            else:
+            # Don't notify yourself that you replied
+            # to yourself, you loner
+            if parent_comment.user.id != g.user.id:
                 self.notifications_to_send.append({
-                    'user': comment.post.user,
+                    'user': parent_comment.user,
                     'payload': payload,
-                    'email_type': 'commented_on_post',
-                    'notification_type': 'someone_comments_on_your_post',
-                    'notification_message': self.render_notification_message('someone_comments_on_your_post')
+                    'email_type': 'replied_to_comment',
+                    'notification_type': 'someone_replies_to_your_comment',
+                    'notification_message': self.render_notification_message('someone_replies_to_your_comment')
                 })
+        # Likewise, you don't want to know that
+        # you commented on your own post
+        elif comment.post.user.id != g.user.id:
+            self.notifications_to_send.append({
+                'user': comment.post.user,
+                'payload': payload,
+                'email_type': 'commented_on_post',
+                'notification_type': 'someone_comments_on_your_post',
+                'notification_message': self.render_notification_message('someone_comments_on_your_post')
+            })
