9e76ea5bb478ef0f89147dcbcf1019983c5940ac
Lewis Monteith <lewismonteith@gmail.com>
2020-09-03 21:44:05 +0100

    Add a basic ui around search

--- hackerstash/views/search.py
+++ hackerstash/views/search.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify, request
+from flask import Blueprint, jsonify, request, render_template
 from hackerstash.lib.universal_search import universal_search
 from hackerstash.utils.auth import login_required
 
@@ -8,5 +8,8 @@ search = Blueprint('search', __name__)
 @search.route('/search')
 @login_required
 def index() -> str:
-    response = universal_search(request.args.get('q'))
-    return jsonify(response)
+    results = universal_search(request.args.get('q'))
+    if request.headers.get('X-Requested-With') == 'fetch':
+        return jsonify(results)
+    else:
+        return render_template('search/index.html', results=results)
