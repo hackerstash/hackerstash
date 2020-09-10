8fac721a94956186c82fe146b6a4544fb89583d0
Lewis Monteith <lewismonteith@gmail.com>
2020-09-10 20:48:40 +0100

    Dont notify yourself that you left a project

--- hackerstash/lib/notifications/types/member_removed.py
+++ hackerstash/lib/notifications/types/member_removed.py
@@ -6,21 +6,23 @@ class MemberRemoved(Base):
         super().__init__(payload)
 
         member = payload['member']
+        remover = payload['remover']
 
         for m in member.project.members:
-            if m.id == member.id:
-                self.notifications_to_send.append({
-                    'user': m.user,
-                    'payload': payload,
-                    'email_type': 'removed_from_project',
-                    'notification_type': 'you_were_removed_from_a_project',
-                    'notification_message': self.render_notification_message('you_were_removed_from_a_project')
-                })
-            else:
-                self.notifications_to_send.append({
-                    'user': m.user,
-                    'payload': payload,
-                    'email_type': 'member_left_project',
-                    'notification_type': 'a_team_member_left_your_project',
-                    'notification_message': self.render_notification_message('a_team_member_left_your_project')
-                })
+            if m.user.id != remover.id:
+                if m.id == member.id:
+                    self.notifications_to_send.append({
+                        'user': m.user,
+                        'payload': payload,
+                        'email_type': 'removed_from_project',
+                        'notification_type': 'you_were_removed_from_a_project',
+                        'notification_message': self.render_notification_message('you_were_removed_from_a_project')
+                    })
+                else:
+                    self.notifications_to_send.append({
+                        'user': m.user,
+                        'payload': payload,
+                        'email_type': 'member_left_project',
+                        'notification_type': 'a_team_member_left_your_project',
+                        'notification_message': self.render_notification_message('a_team_member_left_your_project')
+                    })
