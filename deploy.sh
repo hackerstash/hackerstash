78039bbcf0a89ce706925a6ee55fbdd9784a2523
Harry <hazmeister@gmail.com>
2020-09-14 23:50:28 +0100

    Restart ECS

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+BRANCH=${CODEBUILD_WEBHOOK_TRIGGER:7}  #Remove branch/
+
+if [[ "${BRANCH}" == 'master' ]]; then
+  SERVICE='hackerstash'
+elif if [[ "${BRANCH}" == 'add-stripe' ]]; then
+  SERVICE='hackerstash-staging'
+else
+  exit 0
+fi
+
+aws ecs update-service --cluster hackerstash --service "${SERVICE}" --force-new-deployment
+aws ecs wait services-stable --cluster hackerstash --services "${SERVICE}"
