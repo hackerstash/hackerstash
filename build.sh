dbf416599241ef6a8ff5b93f800f17055f376857
Harry <hazmeister@gmail.com>
2020-09-14 22:58:27 +0100

    Test branch

--- build.sh
+++ build.sh
@@ -1,8 +1,16 @@
 #!/usr/bin/env bash
-BRANCH=${1:7}
-echo $BRANCH
-echo $CODEBUILD_WEBHOOK_TRIGGER
-#docker build -t $IMAGE_REPO_NAME .
-#- docker tag $IMAGE_REPO_NAME ${AWS_ACCOUNT_ID}.dkr.ecr.eu-west-1.amazonaws.com/$IMAGE_REPO_NAME
-#- docker run $IMAGE_REPO_NAME python -m 'scripts.build_assets'
-#- docker push ${AWS_ACCOUNT_ID}.dkr.ecr.eu-west-1.amazonaws.com/$IMAGE_REPO_NAME
+
+BRANCH=${CODEBUILD_WEBHOOK_TRIGGER:7}  #Remove branch/
+
+if [[ "${BRANCH}" == 'master' ]]; then
+  DOCKER_TAG="${IMAGE_REPO_NAME}:latest"
+else
+  DOCKER_TAG="${IMAGE_REPO_NAME}:dev-${BRANCH}"
+fi
+
+echo "$DOCKER_TAG"
+
+docker build -t "$DOCKER_TAG" .
+docker tag "$DOCKER_TAG" "${AWS_ACCOUNT_ID}.dkr.ecr.eu-west-1.amazonaws.com/$DOCKER_TAG"
+docker run "$DOCKER_TAG" python -m 'scripts.build_assets'
+docker push "${AWS_ACCOUNT_ID}.dkr.ecr.eu-west-1.amazonaws.com/$DOCKER_TAG"
