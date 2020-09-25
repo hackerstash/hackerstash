08edc100e7960ec7ee65bf43d9687d78dc02ec37
Lewis Monteith <lewismonteith@gmail.com>
2020-09-25 11:20:21 +0100

    Add phone part of the accordion

--- hackerstash/static/js/home.js
+++ hackerstash/static/js/home.js
@@ -1,11 +1,16 @@
 const accordion = document.querySelector('.accordion');
 
 if (accordion) {
-    accordion.querySelectorAll('.toggle').forEach(element => {
+    accordion.querySelectorAll('li').forEach(element => {
         element.addEventListener('click', event => {
-            accordion.querySelector('.active').classList.remove('active');
+            accordion.querySelectorAll('li').forEach(x => x.classList.remove('active'));
+            document.querySelectorAll('.phone img').forEach(x => x.classList.remove('active'));
 
-            event.target.closest('li').classList.add('active');
+            const item = event.target.closest('li');
+            item.classList.add('active');
+            const tab = item.getAttribute('data-tab');
+            console.log(tab);
+            document.querySelector(`.phone img[data-tab="${tab}"]`).classList.add('active');
         });
     });
 }
