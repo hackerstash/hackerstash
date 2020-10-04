424457b6fd75646dca1a02f5ccf57641467c950d
Hazmeister <hazmeister@gmail.com>
2020-10-04 08:34:02 +0100

    Deploy on new cluster

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -3,12 +3,12 @@
 BRANCH=${CODEBUILD_WEBHOOK_TRIGGER:7}  #Remove branch/
 
 if [[ "${BRANCH}" == 'master' ]]; then
-  SERVICE="hackerstash"
+  CLUSTER="prod"
 elif [[ "${BRANCH}" == 'add-stripe' ]]; then
-  SERVICE="hackerstash-staging"
+  CLUSTER="staging"
 else
   exit 0
 fi
 
-aws ecs update-service --cluster hackerstash --service "${SERVICE}" --force-new-deployment
-aws ecs wait services-stable --cluster hackerstash --services "${SERVICE}"
+aws ecs update-service --cluster "${CLUSTER}" --service "hackerstash " --force-new-deployment
+aws ecs wait services-stable --cluster "${CLUSTER} --services "hackerstash"
