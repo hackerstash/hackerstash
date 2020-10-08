1582ab7e6a2a5f80582a9bfdff9c4344bf171444
Lewis Monteith <lewismonteith@gmail.com>
2020-10-08 08:56:24 +0100

    Delete dstores

--- wsgi.py
+++ wsgi.py
@@ -2,7 +2,7 @@ from werkzeug.middleware.proxy_fix import ProxyFix
 from hackerstash.server import create_app
 
 app = create_app()
-app.wsgi_app = ProxyFix(app.wsgi_app, x_for=2, x_host=2)
+app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_host=1)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
