f2ffdf423279b2f666d8e3c5fda6ef4963479d76
Lewis Monteith <lewismonteith@gmail.com>
2020-10-14 16:24:55 +0100

    Redirect favicon

--- hackerstash/views/home.py
+++ hackerstash/views/home.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, jsonify
+from flask import Blueprint, render_template, jsonify, redirect, url_for
 from hackerstash.db import db
 from hackerstash.lib.logging import Logging
 from hackerstash.lib.redis import redis
@@ -10,6 +10,14 @@ home = Blueprint('home', __name__)
 @home.route('/', methods=['GET', 'POST'])
 def index() -> str:
     return render_template('home/index.html')
+
+
+@home.route('/favicon.ico')
+def favicon():
+    # Not sure where else to put this, but Chrome insists
+    # on requesting it from here regardless of what I put
+    # in the <head> tag
+    return redirect(url_for('static', filename='images/favicon.ico'))
 
 
 @home.route('/__ping')
