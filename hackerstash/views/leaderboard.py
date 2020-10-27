cb6de6040f11c40b382a7aa0d9b1b9211a8f2d57
Lewis Monteith <lewismonteith@gmail.com>
2020-10-27 16:59:54 +0000

    Add skip button on onboarding and leaderboard fixes

--- hackerstash/views/leaderboard.py
+++ hackerstash/views/leaderboard.py
@@ -1,3 +1,4 @@
+import arrow
 from flask import Blueprint, render_template
 from hackerstash.lib.pagination import paginate
 from hackerstash.models.project import Project
@@ -7,7 +8,9 @@ leaderboard = Blueprint('leaderboard', __name__)
 
 @leaderboard.route('/leaderboard')
 def index() -> str:
+    remaining_days = arrow.utcnow().ceil('month').humanize(only_distance=True)
     projects = Project.query.filter_by(published=True).all()
     projects = sorted(projects, key=lambda x: x.position)
     results, pagination = paginate(projects)
-    return render_template('leaderboard/index.html', projects=results, pagination=pagination)
+    data = {'remaining_days': remaining_days, 'projects': results, 'pagination': pagination}
+    return render_template('leaderboard/index.html', **data)
