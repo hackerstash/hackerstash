61f041818e77b2937ad6e651ca5e9a846fe7a1ad
Lewis Monteith <lewismonteith@gmail.com>
2020-11-06 21:18:17 +0000

    Onboarding improvements

--- hackerstash/static/js/modals.js
+++ hackerstash/static/js/modals.js
@@ -4,6 +4,7 @@ openModalButtons.forEach((element) => {
     element.addEventListener('click', (event) => {
         if (event.target.classList.contains('disabled')) return;
 
+        event.preventDefault();
         const selector = event.target.closest('.modal-open').getAttribute('data-modal');
         document.querySelector('#' + selector).classList.add('open');
     });
@@ -12,11 +13,13 @@ openModalButtons.forEach((element) => {
 document.addEventListener('click', (event) => {
     if (event.target.closest('.modal')) {
         if (!event.target.closest('.modal-body')) {
+            event.preventDefault();
             event.target.closest('.modal').classList.remove('open');
         }
     }
 
     if (event.target.closest('.modal-close')) {
+        event.preventDefault();
         event.target.closest('.modal').classList.remove('open');
     }
 });
