ea040c156fc91874435ba42176111b59011b0fbb
Lewis Monteith <lewismonteith@gmail.com>
2020-12-01 07:24:08 +0000

    Fix the leaderboard

--- hackerstash/lib/leaderboard.py
+++ hackerstash/lib/leaderboard.py
@@ -6,20 +6,18 @@ from hackerstash.lib.redis import redis
 log = Logging(module='Leaderboard')
 
 
-def key() -> str:
-    now = datetime.now()
-    return f'monthly_leaderboard:{now.month}:{now.year}'
-
-
 class Leaderboard:
-    key: str = key()
-
     def __init__(self, project):
         """
         Initialise a new instance of the leaderboard class
         :param project:
         """
         self.project = project
+
+    @classmethod
+    def key(cls) -> str:
+        now = datetime.now()
+        return f'monthly_leaderboard:{now.month}:{now.year}'
 
     @classmethod
     def order(cls, reverse=False) -> list[int]:
@@ -33,9 +31,9 @@ class Leaderboard:
         # default we return the ids in descending order
         # as that's the default order for the leaderboard
         if reverse:
-            order = redis.zrange(cls.key, 0, -1)
+            order = redis.zrange(cls.key(), 0, -1)
         else:
-            order = redis.zrevrange(cls.key, 0, -1)
+            order = redis.zrevrange(cls.key(), 0, -1)
         # Decode the ids from bytes and convert to ints
         return [int(x.decode('utf-8')) for x in order]
 
@@ -43,7 +41,7 @@ class Leaderboard:
     def remaining_days(cls) -> str:
         """
         Return how long until the tournament ends in a human
-        readble format
+        readable format
         :return: str
         """
         return arrow.utcnow().ceil('month').humanize(only_distance=True)
@@ -55,7 +53,7 @@ class Leaderboard:
         will return it on a 1-index basis for human consumption
         :return: int
         """
-        rank = redis.zrevrank(self.key, self.project.id)
+        rank = redis.zrevrank(self.key(), self.project.id)
         return rank + 1 if rank is not None else -1
 
     @property
@@ -64,7 +62,7 @@ class Leaderboard:
         Get the score of a single project from the leaderboard
         :return: int
         """
-        score = redis.zscore(self.key, self.project.id) or 0.0
+        score = redis.zscore(self.key(), self.project.id) or 0.0
         return int(score)
 
     def update(self, amount: int) -> int:
@@ -74,6 +72,6 @@ class Leaderboard:
         :param amount: int
         :return: int
         """
-        score = redis.zincrby(self.key, amount, self.project.id)
-        log.info('Updating leaderboard', {'key': self.key, 'amount': amount, 'new_score': int(score), 'project_id': self.project.id})
+        score = redis.zincrby(self.key(), amount, self.project.id)
+        log.info('Updating leaderboard', {'key': self.key(), 'amount': amount, 'new_score': int(score), 'project_id': self.project.id})
         return int(score)
