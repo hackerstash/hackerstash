3dbc408c7e28ea0e6ce5f795206664e6a6abbdbf
Lewis Monteith <lewismonteith@gmail.com>
2021-07-08 15:44:10 +0100

    Trim the codebuild file down

--- bin/build.sh
+++ bin/build.sh
@@ -1,22 +1,7 @@
 #!/usr/bin/env bash
 
-BRANCH=${CODEBUILD_WEBHOOK_TRIGGER:7}  #Remove branch/
-
-if [[ "${BRANCH}" == 'master' ]]; then
-  DOCKER_TAG="${IMAGE_REPO_NAME}:master"
-else
-  DOCKER_TAG="${IMAGE_REPO_NAME}:dev-${BRANCH}"
-fi
-
-echo "$DOCKER_TAG"
+DOCKER_TAG="${IMAGE_REPO_NAME}:master"
 
 docker build -t "${DOCKER_TAG}" .
 docker tag "${DOCKER_TAG}" "${AWS_ACCOUNT_ID}.dkr.ecr.eu-west-1.amazonaws.com/${DOCKER_TAG}"
-docker tag "${DOCKER_TAG}" "${AWS_ACCOUNT_ID}.dkr.ecr.eu-west-1.amazonaws.com/${DOCKER_TAG}-${CODEBUILD_BUILD_NUMBER}"
 docker push "${AWS_ACCOUNT_ID}.dkr.ecr.eu-west-1.amazonaws.com/${DOCKER_TAG}"
-docker push "${AWS_ACCOUNT_ID}.dkr.ecr.eu-west-1.amazonaws.com/${DOCKER_TAG}-${CODEBUILD_BUILD_NUMBER}"
-
-if [[ "${BRANCH}" == 'master' ]]; then
-  docker tag "${DOCKER_TAG}" "${AWS_ACCOUNT_ID}.dkr.ecr.eu-west-1.amazonaws.com/${IMAGE_REPO_NAME}"
-  docker push "${AWS_ACCOUNT_ID}.dkr.ecr.eu-west-1.amazonaws.com/${IMAGE_REPO_NAME}"
-fi
--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,14 +1,4 @@
 #!/usr/bin/env bash
 
-BRANCH=${CODEBUILD_WEBHOOK_TRIGGER:7}  #Remove branch/
-
-if [[ "${BRANCH}" == 'master' ]]; then
-  CLUSTER="prod"
-elif [[ "${BRANCH}" == 'staging' ]]; then
-  CLUSTER="staging"
-else
-  exit 0
-fi
-
-aws ecs update-service --cluster "${CLUSTER}" --service "hackerstash" --force-new-deployment
-aws ecs wait services-stable --cluster "${CLUSTER}" --services "hackerstash"
+aws ecs update-service --cluster hackerstash --service hackerstash --force-new-deployment
+aws ecs wait services-stable --cluster hackerstash --services hackerstash
